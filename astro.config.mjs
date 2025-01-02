e97533e594468de31be3f1c0fbd527e79aee84c8
stacytaylor <stacy.taylor@gmail.com>
2025-01-02 08:33:52 -0800

    created collection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://madebymax.dev"
-
+  site: "https://madebymax.dev",
+  integrations: [preact()]
 });
--- src/blog/post-1.md
+++ src/blog/post-1.md
@@ -1,5 +1,4 @@
 ---
-layout: ../../layouts/MarkdownPostLayout.astro
 
 title: 'My First Blog Post'
 pubDate: 2022-07-01
