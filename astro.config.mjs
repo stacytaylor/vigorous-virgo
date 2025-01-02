f75e8fd76edf454fef7ff8293923581e1ddc16d2
stacytaylor <stacy.taylor@gmail.com>
2025-01-02 08:44:45 -0800

    installed tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://madebymax.dev",
-  integrations: [preact()]
+  integrations: [preact(), tailwind()]
 });
